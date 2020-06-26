966845f51acca568abce340afceb4bfbc2b9971a
Amir <amir@Rocky82-Laptop.localdomain>
2020-06-26 15:53:22 +0700

    Installing Pug

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const path = require('path');
+
+// Init App
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Load view Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 
+// Home Server
+// app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.render('index', {
+    title: 'Heloo'
+}));
+
+// Start Server
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
