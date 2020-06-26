7fa23ab46eb04abb4416875e19bb8499eff2f68b
Amir <amir@Rocky82-Laptop.localdomain>
2020-06-26 22:15:40 +0700

    Static passing data to view

--- app.js
+++ app.js
@@ -3,17 +3,51 @@ const path = require('path');
 
 // Init App
 const app = express();
-const port = 3000
 
 // Load view Engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 // Home Server
 // app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/', (req, res) => res.render('index', {
-    title: 'Heloo'
-}));
+
+app.get('/', function (req, res) {
+    let articles = [
+        {
+            id: 1,
+            title: 'Article One',
+            author: 'Amir',
+            body: 'This is article one'
+        },
+        {
+            id: 2,
+            title: 'Article Two',
+            author: 'Amir',
+            body: 'This is article Two'
+        },
+        {
+            id: 3,
+            title: 'Article Three',
+            author: 'Amir',
+            body: 'This is article Three'
+        }
+    ];
+    res.render('index', {
+    title: 'Articles',
+    articles: articles
+    });
+});
+
+
+// Add Article
+app.get('/articles/add', function (req, res) {
+    res.render('add_article', {
+    title: 'Add Article'
+    });
+});
+
 
 // Start Server
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(3000, function(){
+    console.log('Server started on port 3000...');
+});
