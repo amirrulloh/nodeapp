e588da7d21b7f9aef4fac2de2bb82d6ae72be25f
Amir <amir@Rocky82-Laptop.localdomain>
2020-06-28 11:14:41 +0700

    INstall mongoose

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb',{useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+// Check for db error
+db.on('error', function(err){
+    console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in models
+let Article = require('./models/article');
 
 // Load view Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Home Server
-// app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/', function (req, res) {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article One',
-            author: 'Amir',
-            body: 'This is article one'
-        },
-        {
-            id: 2,
-            title: 'Article Two',
-            author: 'Amir',
-            body: 'This is article Two'
-        },
-        {
-            id: 3,
-            title: 'Article Three',
-            author: 'Amir',
-            body: 'This is article Three'
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index', {
+                title: 'Articles',
+                articles: articles
+            });
         }
-    ];
-    res.render('index', {
-    title: 'Articles',
-    articles: articles
-    });
+    }); 
 });
 
 
